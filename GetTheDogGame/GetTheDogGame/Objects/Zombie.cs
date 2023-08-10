88fa7ae302832b3ab5e0e20bdb6027980eb08d89
Bayram Han Capa <bayramcapa85@gmail.com>
2023-08-10 21:48:40 +0200

    Added basic level needs

--- GetTheDogGame/GetTheDogGame/Objects/Zombie.cs
+++ GetTheDogGame/GetTheDogGame/Objects/Zombie.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using GetTheDogGame.Animations;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace GetTheDogGame.Objects
 {
-	public class Zombie
-	{
-		public Zombie()
-		{
-		}
-	}
+    class Zombie : Enemy
+    {
+        Animation runAnimation, attackAnimation, deathAnimation;
+
+        public Zombie(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
+        {
+            base.speed.X = 2;
+            base.width = 64;
+            base.height = 64;
+            base.scale = 2;
+
+            MakeAnimations();
+
+            animationManager.CurrentAnimation = runAnimation;
+        }
+
+        internal override void MakeAnimations()
+        {
+            attackAnimation = new Animation();
+            attackAnimation.AddSpriteRow(width, height, 0, 9);
+
+            deathAnimation = new Animation();
+            deathAnimation.AddSpriteRow(width, height, 1, 8);
+
+            runAnimation = new Animation();
+            runAnimation.AddSpriteRow(width, height, 2, 4);
+        }
+    }
+
 }
 
